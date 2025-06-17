aac2eadeab7bb7d899de98c877797c8444b5ccb2
Brandon Pinnick <evolvect755@gmail.com>
2025-06-17 04:35:36 +0000

    Replace all implemented animations to more simple animations (fade in/ou

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -77,18 +77,10 @@ const SelectContent = React.forwardRef<
       ref={ref}
       className={cn(
         "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
-        "data-[state=open]:fade-in-0", // Removed animate-in
-        "data-[side=bottom]:data-[state=open]:slide-in-from-top-2",
-        "data-[side=left]:data-[state=open]:slide-in-from-right-2",
-        "data-[side=right]:data-[state=open]:slide-in-from-left-2",
-        "data-[side=top]:data-[state=open]:slide-in-from-bottom-2",
-        "data-[state=closed]:fade-out-0", // Removed animate-out
-        "data-[side=bottom]:data-[state=closed]:slide-out-to-top-2",
-        "data-[side=left]:data-[state=closed]:slide-out-to-right-2",
-        "data-[side=right]:data-[state=closed]:slide-out-to-left-2",
-        "data-[side=top]:data-[state=closed]:slide-out-to-bottom-2",
+        "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
+        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
         position === "popper" &&
-          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
+          "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
         className
       )}
       position={position}
