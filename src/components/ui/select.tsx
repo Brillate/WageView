22c665b868c8487ff01fa8c8eafb1746d1226a60
Brandon Pinnick <evolvect755@gmail.com>
2025-06-17 04:20:05 +0000

    Get rid of the animation for the pay period option and replace it with a

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -75,7 +76,17 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
+        "data-[state=open]:animate-in data-[state=open]:fade-in-0",
+        "data-[side=bottom]:data-[state=open]:slide-in-from-top-2",
+        "data-[side=left]:data-[state=open]:slide-in-from-right-2",
+        "data-[side=right]:data-[state=open]:slide-in-from-left-2",
+        "data-[side=top]:data-[state=open]:slide-in-from-bottom-2",
+        "data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
+        "data-[side=bottom]:data-[state=closed]:slide-out-to-top-2",
+        "data-[side=left]:data-[state=closed]:slide-out-to-right-2",
+        "data-[side=right]:data-[state=closed]:slide-out-to-left-2",
+        "data-[side=top]:data-[state=closed]:slide-out-to-bottom-2",
         position === "popper" &&
           "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
